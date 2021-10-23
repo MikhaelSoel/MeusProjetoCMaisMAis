bfb2e89fbb95d531fd9c1f0e2fe8adc5788b9639
Mikhael Soel <mikhaelvini@gmail.com>
2021-10-23 16:22:16 -0300

    Revert "Adição de comentarios"
    This reverts commit 2f82caa67a7a3fcc0fb4125210a40a72237d26a2.

--- PrimeiroCodigo.cpp
+++ PrimeiroCodigo.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-//A diretiva de pré processador #include
-// Indica para incluir o arquivo de cabeçalho
-// iostream
-//"namespace" Espaço de nome
 int main()
-// Função principal. Todo programa começa com esta função principal mein
-{//Indica o inicio de um bloco de codigo
-//cout é um comando de saida de dados, neste caso está solicitando para colocar na saida padrão(tela) a frase especificada
-// endl inidca para inserir um caracter de quebra de linha na frase("string")
+{
     std::cout << "Primeiro codigo, Olá mundo!!!" << std::endl;
     system("PAUSE");
-    //Chama o shell do windos e coloca este pause
     return 0;
-}// Indica o fim de um bloco de codigo
+}
