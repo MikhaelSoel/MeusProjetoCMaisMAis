9f55e7e7bf18cdb438acac011ea9583ff19b918e
Mikhael Soel <mikhaelvini@gmail.com>
2021-10-25 16:47:13 -0300

    EuVouAprender.cpp

---
+++ EuVouAprender.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    std::cout <<"Eu"
+    <<std::endl;
+    std::cout <<"Vou"
+    <<std::endl;
+    std::cout <<"Apender"
+    <<std::endl;
+    std::cout <<"C++"
+    <<std::endl;
+
+    std::cout << "Eu"  << std::endl << "vou" << std::endl << "Aprender" << std::endl << "C++" << std::endl;
+
+   
+    system("PAUSE");
+    return 0;
+}
--- PrimeiroCodigo.cpp
+++ PrimeiroCodigo.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+//Diretiva de pre processador indica para incluir o arquivo de cabeçalho
+//IOsstream pede para anexxar todo o codigo
 int main()
-{
+//Função principal main, todo programa em c++ comecça com essa função
+{ //indica o inico de um bloco de codigo
+//cout é um comando de saida de dados, neste caso solicita que saida padrão, a tela, ponha a frase especificada.
+//endl indcica para inserir um caracter de quebra de linha("String")
+//system pause chama o shell do windowns, propmt
     std::cout << "Primeiro codigo, Olá mundo!!!" << std::endl;
+    system("color 5F");
     system("PAUSE");
     return 0;
-}
+    //retorna para o sistema opercaional zero
+    //zero é o padrão para informa que tudo deu certo, assim se a função main de certo, então ela retornara
+}//indica o fim de um bloco
