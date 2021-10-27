1d7c02c21c16e131ef44b5739efc6e5a686c05f1
Mikhael Soel <mikhaelvini@gmail.com>
2021-10-27 20:31:14 -0300

    Tipo de Variaveis

---
+++ TiposVariaveis.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int Numero;
+    Numero = 45;
+    std::cout <<  "Valor número: " << Numero << std::endl;
+    std::cout << " Tamanho da variavel numero: " << sizeof(Numero) << "\n";
+    std::cout << " Endereço da memoria: " << &Numero << "\n";
+    system("PAUSE");
+}
