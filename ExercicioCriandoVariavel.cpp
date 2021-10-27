081869e10ab7fced5ea6474470cd8cf013f57c19
Mikhael Soel <mikhaelvini@gmail.com>
2021-10-27 20:49:38 -0300

    Create ExercicioCriandoVariavel.cpp

---
+++ ExercicioCriandoVariavel.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int NumComputadores;
+    int SerialComputadores;
+    NumComputadores = 100;
+    SerialComputadores = 1111;
+    std::cout << "\t$$$$$$$$$$$$$$$$$$" << std::endl;
+    std::cout << "Valor de NumComputadores: " << NumComputadores << "\n";
+    std::cout << "Valor de serial computadores: " << SerialComputadores << "\n";
+    std::cout << "\t$$$$$$$$$$$$$$$$$$" << std::endl;
+    system("PAUSE");
+    return 0;
+}
