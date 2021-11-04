ed94535ddcc3afa2cd6b7e7df6d802ef4a622800
Mikhael Soel <mikhaelvini@gmail.com>
2021-11-04 00:06:14 -0300

    Complemento

--- ValorBoleano.cpp
+++ ValorBoleano.cpp
@@ -4,5 +4,17 @@ int main()
     bool achou;
     achou = 1;
     std::cout << "Valor achou: " << achou << std::endl;
+    achou = 0;
+    std::cout << "Valor achou: " << achou << std::endl;
+    achou = true;
+    std::cout << "Valor achou: " << achou << std::endl;
+    achou = false;
+    std::cout << "Valor achou: " << achou << std::endl;
+    achou = 'd';
+    std::cout << "Valor achou: " << achou << std::endl;
+    system("PAUSE");
     return 0;
+
+    //Qualquer coisa diferente de zero é verdadeiro
+    
 }
