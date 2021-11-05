951a49f6dbbe33c043df579a7ba7f22ff9bf644a
Mikhael Soel <mikhaelvini@gmail.com>
2021-11-05 01:48:33 -0300

    Apaguei as notas dos outros exer

--- ModificadoresDETIpo.cpp
+++ ModificadoresDETIpo.cpp
@@ -2,3 +2,22 @@
 //unsigned (Sem sinal)
 //Long (Expande as faixas de valores)
 //Short (Contrai a faixa de valores)
+//Tendi nada e não roda
+#include <iostream>
+
+int main()
+{
+	std::cout << "Tamanho char : " << sizeof(char) << std::endl;
+	std::cout << "Tamanho int : " << sizeof(int) << std::endl;
+	std::cout << "Tamanho short int : " << sizeof(short int) << std::endl;
+	std::cout << "Tamanho long int : " << sizeof(long int) << std::endl;
+	std::cout << "Tamanho long long int : " << sizeof(long long int) << std::endl;
+	std::cout << "Tamanho float : " << sizeof(float) << std::endl;
+	std::cout << "Tamanho float : " << sizeof(long float) << std::endl;
+	std::cout << "Tamanho double : " << sizeof(double) << std::endl;
+	std::cout << "Tamanho long double : " << sizeof(long double) << std::endl;
+	std::cout << "Tamanho wchar_t : " << sizeof(wchar_t) << std::endl;
+	system("PAUSE");
+
+	return 0;
+}
