e59f9fb53ab4f0f76388470c97cbc355072b2c21
Mikhael Soel <mikhaelvini@gmail.com>
2021-11-05 01:59:15 -0300

    Criação do codigo

---
+++ Constante.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main ()
+{
+    const[tipo][nome] = [valor];
+    const NUM_VIDAS = 10; //cria uma região na memirua
+    const int BONUS = 200; //Cria uma constante inteira que armazena 200
+    const double PI = 3.1415; //Uma constante ponto flutuante de dupla precisão que armazena o valor aproximado de pi
+    #define NUM_VIDAS 10; // PODE ser usado para criar constantes mas existem difernças, é uma diretiva de pre processador de codigo. Não cria nada na meemoria
+}
