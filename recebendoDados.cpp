1a094c5addab566760f5118c35be67dafaa5dff5
Mikhael Soel <mikhaelvini@gmail.com>
2021-11-08 03:51:39 -0300

    Entrada de dados

---
+++ recebendoDados.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main ()
+{
+    int numero01, numero02;
+    std::cout <<"Digite o primerio numero: " << "\n";
+    std::cin >> numero01;
+    std::cout << "Numero01  = " << numero01 << std::endl;
+    system("PAUSE");
+
+}
